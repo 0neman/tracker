d25f3bcbb6a62e5f377ad2bab46726d803bb2986
unknown <anishossameddine.belalia@univ-tiaret.dz>
2025-02-15 21:37:39 +0100

    minor stuffs

--- lib/components/history_row.dart
+++ lib/components/history_row.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HistoryRow extends StatelessWidget {
-  const HistoryRow({super.key});
+  final List<String> details;
+  const HistoryRow({super.key, required this.details});
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    var something = List<Widget>.generate(
+      details.length,
+      (index) => Row(
+        children: [
+          Text(details.elementAt(index)),
+        ],
+      ),
+    );
+    print(something);
+    return Card(
       child: ExpansionTile(
         title: Text("Colique"),
         subtitle: Text("Colique Spasmodique"),
-        children: [],
+        children: something,
       ),
     );
   }
--- lib/screens/horse_details_screen.dart
+++ lib/screens/horse_details_screen.dart
@@ -29,64 +29,34 @@ class HorseDetailsScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
             width: double.maxFinite,
             height: 300,
-            child: Stack(
-              children: [
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: InkResponse(
-                    containedInkWell: true,
+            child: ListView.separated(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 5,
+              ),
+              itemBuilder: (contex, index) => Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.6),
+                        offset: const Offset(5, 10),
+                        blurRadius: 10,
+                        spreadRadius: -7,
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
-                    onTap: () => 1 + 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withValues(alpha: 0.30),
-                            offset: const Offset(1, 5),
-                            blurRadius: 10,
-                            spreadRadius: -7,
-                          ),
-                        ],
-                        borderRadius: const BorderRadius.only(
-                          bottomRight: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                      ),
-                      width: 90,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.add_box_rounded),
-                        ),
-                      ),
+                    child: Image.network(
+                      'https://images.unsplash.com/uploads/14136148007774dc82563/ce92d553?q=80&w=1046&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withValues(alpha: 0.6),
-                          offset: const Offset(5, 10),
-                          blurRadius: 10,
-                          spreadRadius: -7,
-                        ),
-                      ],
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.network(
-                        'https://images.unsplash.com/uploads/14136148007774dc82563/ce92d553?q=80&w=1046&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
           const Column(
@@ -126,7 +96,9 @@ class HorseDetailsScreen extends StatelessWidget {
           Container(
             height: MediaQuery.sizeOf(context).height * 0.7,
             child: ListView.separated(
-              itemBuilder: (context, index) => HistoryRow(),
+              itemBuilder: (context, index) => HistoryRow(
+                details: ['somethi', 'some'],
+              ),
               itemCount: 10,
               separatorBuilder: (context, index) => SizedBox(
                 height: 5,
